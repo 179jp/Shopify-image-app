91936e5c0ec6f9ce4586bd92e1ad17b632212f4c
nnjyami <y@179.jp>
2024-10-23 18:25:47 +0900

    フィルタの実装を追加

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { json } from "@remix-run/node";
 import { useFetcher } from "@remix-run/react";
 import {
+  Icon,
   Page,
   Layout,
   Text,
@@ -14,6 +15,7 @@ import {
   Link,
   InlineStack,
 } from "@shopify/polaris";
+import { SearchIcon, SortIcon } from "@shopify/polaris-icons";
 import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
 import { useLoaderData } from "react-router-dom";
 import { authenticate } from "../shopify.server";
@@ -24,13 +26,117 @@ import { ImageDetailPage } from "../components/ImageDetailPage";
 import prisma from "../db.server";
 
 import "../components/default.css";
+import "../components/imageFilter.css";
+import { boxShadowLv2 } from "../components-styled/config";
+
+const pageWrapStyle = {
+  backgroundColor: "rgb(248, 248,248)",
+  boxShadow: boxShadowLv2,
+  borderRadius: ".75rem",
+  padding: "20px",
+};
 
 export const loader = async ({ request }) => {
   const { admin } = await authenticate.admin(request);
-  // 最新の50件のファイルを取得する
   const filesResponse = await admin.graphql(`
     {
-      files(first: 50) {
+      files(first: 100) {
+        nodes {
+          id
+          createdAt
+            ... on MediaImage {
+              id
+              image {
+                id
+                originalSrc
+                width
+                height
+              }
+            }
+        }
+      }
+    }`);
+  const {
+    data: {
+      files: { nodes },
+    },
+  } = await filesResponse.json();
+  // コレクションを取得する
+  const collectionResponse = await admin.graphql(`
+    {
+      collections(first: 200) {
+        nodes {
+          id
+          title
+          products(first: 50) {
+            nodes {
+              id
+              title
+            }
+          }
+        }
+      }
+    }`);
+  const {
+    data: {
+      collections: { nodes: collections },
+    },
+  } = await collectionResponse.json();
+
+  // MediaObeject の image_settings を取得する
+  const mediaObjectType = "image_settings";
+  const imageSettingsResponse = await admin.graphql(`
+    { 
+    metaobjects(type:"${mediaObjectType}", first: 100) {
+      edges {
+        node {
+          id
+          type
+          handle
+          fields {
+            key
+            value
+            type
+            reference {
+              __typename
+              ... on Product {
+                id
+                title
+              }
+              ... on Collection {
+                id
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+  }`);
+  const {
+    data: {
+      metaobjects: { edges: imageSettings },
+    },
+  } = await imageSettingsResponse.json();
+
+  return json({
+    files: nodes,
+    images: imageSettings,
+  });
+};
+
+export const action = async ({ request }) => {
+  // URL から filter を取得する
+  const body = await request.formData();
+  const filterName = body.get("name");
+  const sortBy = body.get("sortBy") ? body.get("sortBy") : "first";
+
+  const isSortReverse = sortBy === "last" ? true : false;
+  const filenameQuery = filterName ? `filename:${filterName}` : "";
+  const { admin } = await authenticate.admin(request);
+  const filesResponse = await admin.graphql(`
+    {
+      files(first: 100, sortKey: CREATED_AT, reverse: ${isSortReverse}, query: "${filenameQuery}") {
         nodes {
           id
           createdAt
@@ -76,75 +182,9 @@ export const loader = async ({ request }) => {
   // DB から images を取得
   // const images = await prisma.image.findMany();
   const images = await prisma.Image.findMany();
-
   return json({
     files: nodes,
     images,
-  });
-};
-
-export const action = async ({ request }) => {
-  const { admin } = await authenticate.admin(request);
-  const color = ["Red", "Orange", "Yellow", "Green"][
-    Math.floor(Math.random() * 4)
-  ];
-  const response = await admin.graphql(
-    `#graphql
-      mutation populateProduct($input: ProductInput!) {
-        productCreate(input: $input) {
-          product {
-            id
-            title
-            handle
-            status
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  price
-                  barcode
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }`,
-    {
-      variables: {
-        input: {
-          title: `${color} Snowboard`,
-        },
-      },
-    },
-  );
-  const responseJson = await response.json();
-  const product = responseJson.data.productCreate.product;
-  const variantId = product.variants.edges[0].node.id;
-  const variantResponse = await admin.graphql(
-    `#graphql
-    mutation shopifyRemixTemplateUpdateVariant($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
-      productVariantsBulkUpdate(productId: $productId, variants: $variants) {
-        productVariants {
-          id
-          price
-          barcode
-          createdAt
-        }
-      }
-    }`,
-    {
-      variables: {
-        productId: product.id,
-        variants: [{ id: variantId, price: "100.00" }],
-      },
-    },
-  );
-  const variantResponseJson = await variantResponse.json();
-
-  return json({
-    product: responseJson.data.productCreate.product,
-    variant: variantResponseJson.data.productVariantsBulkUpdate.productVariants,
   });
 };
 
@@ -159,7 +199,21 @@ export default function Index() {
     "",
   );
 
-  const { files, collections, images } = useLoaderData();
+  // Filter
+  const [fileSort, setFileSort] = useState("last");
+  const [fileName, setFileName] = useState(null);
+  const handleFilterChange = (e) => {
+    const formData = new FormData(e.currentTarget);
+    const newQuery = {
+      name: formData.get("name") || "",
+      sortBy: formData.get("sortBy") || "",
+    };
+
+    // 更新されたクエリでリクエストを送信
+    submit(newQuery, { method: "get", action: "" });
+  };
+
+  const { files, collections, images } = fetcher.data ?? useLoaderData();
 
   const [selectedFile, setSelectedFile] = useState(null);
   const handleSelection = useCallback((file) => {
@@ -197,11 +251,44 @@ export default function Index() {
   return (
     <Page
       fullWidth
-      title="Image Handle App"
+      title="Images"
       subtitle="ファイルに登録されている内容が表示されます"
       compactTitle
     >
-      <ImagesCards files={files} handleSelection={handleSelection} />
+      <div style={pageWrapStyle}>
+        <div className="imageFilter">
+          <fetcher.Form method="post">
+            <ul className="imageFilter_params">
+              <li>
+                <Icon source={SearchIcon} tone="base" />
+                <input type="text" name="name" placeholder="ファイル名" />
+                <button type="submit">検索</button>
+              </li>
+              <li>
+                <Icon source={SortIcon} tone="base" />
+                <select
+                  name="sortBy"
+                  onChange={(e) => {
+                    setFileSort(e.target.value);
+                    fetcher.submit(
+                      {
+                        sortBy: e.target.value,
+                      },
+                      {
+                        method: "POST",
+                      },
+                    );
+                  }}
+                >
+                  <option value="first">新しい順</option>
+                  <option value="last">古い順</option>
+                </select>
+              </li>
+            </ul>
+          </fetcher.Form>
+        </div>
+        <ImagesCards files={files} handleSelection={handleSelection} />
+      </div>
     </Page>
   );
 }
