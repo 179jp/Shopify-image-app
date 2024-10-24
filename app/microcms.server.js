ab7b31f029d833414aa2b030f664c5d36134c870
nnjyami <y@179.jp>
2024-10-24 14:29:10 +0900

    microCMS周りの設定削除

--- app/microcms.server.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "microcms-js-sdk";
-
-export const microcmsClient = createClient({
-  serviceDomain: "yah2903fh1", // service-domain は https://XXXX.microcms.io の XXXX 部分
-  apiKey: process.env.MICROCMS_API_KEY,
-});
