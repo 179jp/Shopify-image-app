d0bbb1ce149eca605638befc465e39ae44d20e53
nnjyami <y@179.jp>
2024-11-02 00:54:37 +0900

    Liquidとの繋ぎ込み部分 実装

---
+++ extensions/extension-app/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+const appRoot = document.getElementById("root");
+const mode = appRoot ? appRoot.getAttribute("data-mode") : "";
+const handle = appRoot ? appRoot.getAttribute("data-handle") : "";
+
+createRoot(appRoot).render(
+  <StrictMode>
+    <App mode={mode} handle={handle} />
+  </StrictMode>,
+);
